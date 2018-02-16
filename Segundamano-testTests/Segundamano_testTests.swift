5b8f0078644f5c46f098406f57905919cc042b07
carmen-salvador <carmen.salvador@schibsted.com.mx>
2018-02-16 12:08:42 -0600

    Initial Commit

---
+++ Segundamano-testTests/Segundamano_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  Segundamano_testTests.swift
+//  Segundamano-testTests
+//
+//  Created by Carmen Salvador Bucio on 2/16/18.
+//  Copyright © 2018 Segundamano.mx. All rights reserved.
+//
+
+import XCTest
+@testable import Segundamano_test
+
+class Segundamano_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
