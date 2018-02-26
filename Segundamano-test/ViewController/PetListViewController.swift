765c817c192faf998783f3d102cc660c4860b756
Adriana Sandoval <adry@m22.mx>
2018-02-26 13:11:25 -0600

    show dog names in list from url = https://dog.ceo/api/breeds/list

--- Segundamano-test/ViewController/PetListViewController.swift
+++ Segundamano-test/ViewController/PetListViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class PetListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let animals = ["A","B","C"]
+    
+    @IBOutlet weak var uitableview: UITableView!
+
+    final let urlDogNames = "https://dog.ceo/api/breeds/list"
+    var dogNameArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.getDogNames()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -19,14 +25,38 @@ class PetListViewController: UIViewController, UITableViewDelegate, UITableViewD
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+//MARK: TABLEVIEW CONFIG
+    func getDogNames (){
+        let url = NSURL(string: urlDogNames)
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                if (jsonObj!.value(forKey: "status") as? String == "success"){
+                    if let dogsNameArray = jsonObj!.value(forKey: "message") as? NSArray {
+                        for dogName in dogsNameArray{
+                            self.dogNameArray.append(dogName as! String)
+                        }
+    //                    print (self.dogNameArray)
+                    }
+                    OperationQueue.main.addOperation({
+                        self.uitableview.reloadData()
+                    })
+                }
+                else {
+                    self.dogNameArray = ["No se pudo obtener información"]
+                }
+            }
+        }).resume()
+    }
+
+//MARK: TABLEVIEW CONFIG
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (animals.count)
+        return dogNameArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
-        cell.uiNameDog.text = animals[indexPath.row]
+        cell.uiNameDog.text = dogNameArray[indexPath.row]
         cell.uiImageDog.image = nil
         return cell
     }
